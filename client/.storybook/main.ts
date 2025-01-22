ee9001c4fff51acaf760b0f23589a6fa40af4ab5
PoeySK <piz115@naver.com>
2025-01-22 14:24:34 +0900

    upload: client 초기 환경 업로드
    Yarn Berry + Vite + React + TypeScript
    
    ESLint + Prettier

---
+++ client/.storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import { join, dirname } from 'path';
+
+function getAbsolutePath(value: string): string {
+  const modulePath = require.resolve(join(value, 'package.json'));
+  return dirname(modulePath);
+}
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {},
+  },
+};
+
+export default config;
---
+++ client/types/eslint/eslint-config-airbnb.d.ts
@@ -0,0 +1,2 @@
+// type으로 전환 후 사용
+declare module 'eslint-config-airbnb';
