c06fa75c4dd46840e10d7720edde60dbd65cdcc2
PoeySK <piz115@naver.com>
2025-01-31 16:57:26 +0900

    move: 노트북에서 데스크탑으로 이동
    나만의 커밋 읽을 필요없음

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@/assets/styles/globals.scss';
 import App from './App.tsx';
+import Header from './components/molecules/Header/index.tsx';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 10,
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Header />,
+    children: [
+      {
+        index: true,
+        element: <Footer />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
