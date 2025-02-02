3f56be0738ac009458a5cc7f18ee86b7f90f2bab
PoeySK <piz115@naver.com>
2025-02-02 15:39:01 +0900

    S12P11B102-201 feat: header route 기능 추가 및 페이지별 아이콘 버튼 유무 설정
    location을 활용해 현재 페이지의 상태에 따라 아이콘이 보여지는게 다름

--- client/src/components/molecules/Header/index.tsx
+++ client/src/components/molecules/Header/index.tsx
@@ -2,13 +2,46 @@ import styles from './index.module.scss';
 import IconButton from '@/components/atoms/IconButton';
 import { ICON_NAME } from '@/constants/ui.constants';
 import IconLogo from '@/assets/icons/IconLogo';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Header() {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const nonDoodleBack = ['/list', '/elections/:election_id'];
+  const nonMyPage = ['/mypage', '/elections/:election_id', '/home'];
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  function handleMyPage() {
+    navigate('/mypage');
+  }
+
+  function handleHome() {
+    navigate('/elections');
+  }
+
   return (
     <nav id={styles.header}>
-      <IconButton name={ICON_NAME.DOODLEBACK} />
-      <IconLogo />
-      <IconButton name={ICON_NAME.MYPAGE} />
+      <div
+        className={
+          nonDoodleBack.some((path) => path === pathname) ? styles.hide : ''
+        }
+      >
+        <IconButton name={ICON_NAME.DOODLEBACK} onClick={handleBack} />
+      </div>
+      <div onClick={handleHome}>
+        <IconLogo />
+      </div>
+      <div
+        className={
+          nonMyPage.some((path) => path === pathname) ? styles.hide : ''
+        }
+      >
+        <IconButton name={ICON_NAME.MYPAGE} onClick={handleMyPage} />
+      </div>
     </nav>
   );
 }
