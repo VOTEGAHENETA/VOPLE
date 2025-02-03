be0cf5b9a097599ea2401fab0c997c0cb922d8a1
PoeySK <piz115@naver.com>
2025-02-03 16:04:06 +0900

    fix: useQuery 오류 해결
    api 연동 시 useQuery 구현 예정정

--- client/src/components/atoms/CircleButton/index.tsx
+++ client/src/components/atoms/CircleButton/index.tsx
@@ -3,60 +3,72 @@ import IconVoteHand from '@/assets/icons/IconVoteHand';
 import useTimer from '@/hooks/useTimer';
 import { useEffect, useState } from 'react';
 import Text from '@/components/atoms/Text';
-import { useQuery } from '@tanstack/react-query';
-import { ElectionContents } from '@/types/election';
-import { getElection } from '@/services/election';
 
 type VoteButtonLabel = '투표하기' | '투표시작전' | '선·관·위';
 
 interface Props {
   /** 버튼 타입 지정 (button, submit, reset) */
   type: 'button' | 'submit' | 'reset';
-  /** 버튼의 활성 상태 (able: true, disable: false) */
-  status: boolean;
   /** 버튼에 이벤트 지정 */
   onClick?: () => void;
 }
 
+// API 연동을 시작하면 바꿀 예정
+// const sessionId = '123';
+const currentUserId = 456789;
+const electionData = {
+  sessionId: '456',
+  hostId: 1234,
+  voteStartTime: '2025-02-03T15:55:50',
+  voteEndTime: '2025-02-03T18:00:00',
+};
+
 /** 메인화면 footer에 적용될 Router 버튼 입니다. */
-function CircleButton({ type = 'button', status, onClick }: Props) {
+function CircleButton({ type = 'button', onClick }: Props) {
+  const [status, setStatus] = useState<boolean>();
   const [buttonLabel, setButtonLabel] = useState<VoteButtonLabel>('투표시작전');
-  const [deadLine, setDeadline] = useState<Date>(new Date()); // test 데이터
+  const [deadLine, setDeadline] = useState<Date>(new Date());
   const timeLeft = useTimer(deadLine);
 
-  // sessionId와 currentUserId는 API 연동을 시작하면 바꿀 예정 (아마 zustand 활용?)
-  const sessionId = '123';
-  const currentUserId = 456789;
-
-  const { data: electionData } = useQuery<ElectionContents>({
-    queryKey: ['election', sessionId],
-    queryFn: () => getElection(sessionId),
-  });
+  // const { data: electionData } = useQuery<ElectionContents>({
+  //   queryKey: ['election', sessionId],
+  //   queryFn: () => getElection(sessionId),
+  // });
 
   useEffect(() => {
-    if (!electionData) return;
-
     const now = new Date();
     const startTime = new Date(electionData.voteStartTime);
     const endTime = new Date(electionData.voteEndTime);
 
     if (electionData.hostId === currentUserId) {
-      setButtonLabel('선·관·위');
-    } else {
-      if (now < startTime) {
-        setButtonLabel('투표시작전');
-        setDeadline(new Date(electionData.voteStartTime));
-      } else if (startTime <= now && now <= endTime) {
+      if (buttonLabel !== '선·관·위') setButtonLabel('선·관·위');
+      return;
+    }
+
+    if (timeLeft === '00:00:00') {
+      if (now <= endTime) {
         setButtonLabel('투표하기');
-        setDeadline(new Date(electionData.voteEndTime));
+        setDeadline(endTime);
+        setStatus(true);
       }
     }
-  }, [electionData]);
 
-  const btnClasses = [
-    styles.btn,
-    styles[`btn-status-${status ? 'able' : 'disable'}`],
-  ].join(' ');
+    if (now < startTime) {
+      if (buttonLabel !== '투표시작전') {
+        setButtonLabel('투표시작전');
+        setDeadline(startTime);
+        setStatus(false);
+      }
+    } else if (now >= startTime && now <= endTime) {
+      if (buttonLabel !== '투표하기') {
+        setButtonLabel('투표하기');
+        setDeadline(endTime);
+        setStatus(true);
+      }
+    }
+  }, [timeLeft]);
+
+  const btnClasses = [styles.btn, styles[`btn-status-${status}`]].join(' ');
 
   return (
     <div className={styles['btn-container']}>
