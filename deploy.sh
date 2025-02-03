691bb4a0137b9b37dbc69de332521c9d43ca588c
황규현 <hwgyuhyeon@gmail.com>
2025-02-03 03:39:00 +0000

    chore: 빌드 프로세스 수정

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
-REPO_URL="https://lab.ssafy.com/s12-webmobile1-sub1/S12P11B102.git"
-BRANCH="master"
-
-echo "🔹 Git 사용자 설정"
-git config user.email "hwgyuhyeon@gmail.com"
-git config user.name "황규현"
-
 echo "🔹 EC2에서 Git 리포지토리 업데이트 중..."
-git pull origin $BRANCH
+git pull origin master
 
 # Docker Compose 실행
 echo "🚀 Docker Compose 실행 중..."
-ls -al
-ls -al client/
 docker compose up -d server client
