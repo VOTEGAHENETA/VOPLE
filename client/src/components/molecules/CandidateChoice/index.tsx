229e4fbebdf84bf7482587c88f8593b1ce1bb689
김선명 <keemsnmyn@naver.com>
2025-02-03 14:03:38 +0900

    S12P11B102-205-feat: 후보자 선택 molecule 구현

---
+++ client/src/components/molecules/CandidateChoice/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import styles from './index.module.scss';
+import Poster from '@/components/atoms/Poster';
+import Text from '@/components/atoms/Text';
+import { mockCandidate } from '../CandidateSection/mockData';
+import check from '@/assets/icons/check.svg';
+
+function CandidateChoice() {
+  const [choice, setChoice] = useState(false);
+
+  const chooseCandidate = () => {
+    setChoice((prev) => !prev);
+  };
+
+  return (
+    <div className={styles.choice}>
+      <div className={styles['choice-poster']} onClick={chooseCandidate}>
+        <Poster size='s' src={mockCandidate.poster} />
+        {choice ? (
+          <img src={check} alt='' className={styles['choice-check']} />
+        ) : null}
+      </div>
+      <Text size='s' weight='bold' color='#000000'>
+        김미리
+      </Text>
+    </div>
+  );
+}
+
+export default CandidateChoice;
