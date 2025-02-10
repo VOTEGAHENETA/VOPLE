77c010f10ffe9799148acf4000a0667727e4f6f1
황규현 <hwgyuhyeon@gmail.com>
2025-02-10 10:15:27 +0900

    fix: 프로젝트 빌드 설정 변경

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-echo "🔹 EC1에서 Git 리포지토리 업데이트 중..."
+echo "🔹 EC2에서 Git 리포지토리 업데이트 중..."
 git pull origin master
 
 # Docker Compose 실행
@@ -9,7 +9,19 @@ docker compose up -d --build server
 docker compose up -d server client
 
 echo "서비스 시작 대기 중..."
-sleep 80
+sleep 90
+for i in {1..3}; do
+    if curl -f http://i12b102.p.ssafy.io; then
+        break
+    fi
+    if [ $i -eq 3 ]; then
+        echo "클라이언트가 응답하지 않습니다."
+        docker logs client
+        exit 1
+    fi
+    echo "클라이언트 health check 재시도 중... ($i/3)"
+    sleep 10
+done
 
 for i in {1..3}; do
     if curl -f http://i12b102.p.ssafy.io/api/test; then
@@ -24,18 +36,6 @@ for i in {1..3}; do
     sleep 10
 done
 
-for i in {1..3}; do
-    if curl -f http://i12b102.p.ssafy.io; then
-        break
-    fi
-    if [ $i -eq 3 ]; then
-        echo "클라이언트가 응답하지 않습니다."
-        docker logs client
-        exit 1
-    fi
-    echo "클라이언트 health check 재시도 중... ($i/3)"
-    sleep 10
-done
 
 rm .env
 rm ./server/src/main/resources/application-prod.yml
