44472997475587e9d36937dc18a0eb805bc6d821
PoeySK <piz115@naver.com>
2025-02-11 16:06:06 +0900

    hotfix: 불필요한 값 제거

--- client/src/pages/Election/Manage/index.tsx
+++ client/src/pages/Election/Manage/index.tsx
@@ -1,12 +1,7 @@
 import ElectionDetailTemplate from '@/components/templates/ElectionDetailTemplate';
 import styles from './index.module.scss';
-import { useCandidateStore } from '@/stores/candidateStore';
-import { useParams } from 'react-router-dom';
-import CandidateRegisterTemplate from '@/components/templates/CandidateRegisterTemplate';
 
 function Manage() {
-  const { election_id } = useParams() as { election_id: string };
-  const { openCandidateModal } = useCandidateStore();
   return (
     <div className={styles['detail-page']}>
       <ElectionDetailTemplate />
