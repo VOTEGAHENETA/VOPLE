95ca6c499e99e9fa7faf4df7b0e84761f735a78e
dongs <don0421865@gmail.com>
2025-02-11 17:01:54 +0900

    S12P11B102-271 feat: candidates list 검색 기능 추가

--- server/src/main/java/com/votegaheneta/vote/service/VoteFindServiceImpl.java
+++ server/src/main/java/com/votegaheneta/vote/service/VoteFindServiceImpl.java
@@ -3,7 +3,6 @@ package com.votegaheneta.vote.service;
 import com.votegaheneta.common.component.SearchComponent;
 import com.votegaheneta.common.component.VoteResultCalculator;
 import com.votegaheneta.common.repository.RedisRepository;
-import com.votegaheneta.vote.dto.CandidateSearchDto;
 import com.votegaheneta.vote.dto.SessionFinalResultFindDto;
 import com.votegaheneta.vote.dto.SessionFinalResultFindDto.Elected;
 import com.votegaheneta.vote.dto.SessionFinalResultFindDto.ElectionSessionDto;
@@ -13,6 +12,7 @@ import com.votegaheneta.vote.dto.SessionResultFindDto;
 import com.votegaheneta.vote.dto.SessionResultFindDto.VoteResult;
 import com.votegaheneta.vote.dto.SessionResultFindDto.VoteResult.TeamResult;
 import com.votegaheneta.vote.dto.VoteDetailDto;
+import com.votegaheneta.vote.dto.VoteInfoDto;
 import com.votegaheneta.vote.entity.ElectionSession;
 import com.votegaheneta.vote.entity.Vote;
 import com.votegaheneta.vote.entity.VoteTeam;
@@ -66,14 +66,13 @@ public class VoteFindServiceImpl implements VoteFindService {
   }
 
   @Override
-  public CandidateSearchDto findSearchCandidates(Long sessionId, Long voteId, String keyword, Pageable pageable) {
+  public List<VoteInfoDto> findSearchCandidates(Long voteId, String keyword, Pageable pageable) {
     // 찾아올때 어떤 데이터를 들고와야할까?
     StringBuilder sb = new StringBuilder();
     for (String word : keyword.split("")) {
       sb.append(searchComponent.searchWordReSetting(word));
     }
-    
-    return null;
+    return customCandidateRepository.findSearchCandidatesBySessionId(voteId, sb.toString(), pageable);
   }
 
   @Override
