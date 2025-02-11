57e8642bdae38570ea6fdf038f12de01485ca234
PoeySK <piz115@naver.com>
2025-02-11 15:41:45 +0900

    chore: param -> sessionId 로 파라미터 네이밍 변경경

--- client/src/services/hooks/useQRCode.ts
+++ client/src/services/hooks/useQRCode.ts
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { getQRCode } from '../election';
 
-export const useQRCode = (param: number) => {
+export const useQRCode = (sessionId: number) => {
   return useQuery({
-    queryKey: ['qr', param],
-    queryFn: () => getQRCode(param),
+    queryKey: ['qr', sessionId],
+    queryFn: () => getQRCode(sessionId),
   });
 };
