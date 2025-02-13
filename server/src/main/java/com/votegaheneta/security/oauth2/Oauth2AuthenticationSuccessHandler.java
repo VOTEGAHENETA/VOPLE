3f72e530a45a5315ac7e120d6765eb4d29abb73d
최효재 <hyojae5945@gmail.com>
2025-02-13 17:28:12 +0900

    fix: redirect URL 수정

--- server/src/main/java/com/votegaheneta/security/oauth2/Oauth2AuthenticationSuccessHandler.java
+++ server/src/main/java/com/votegaheneta/security/oauth2/Oauth2AuthenticationSuccessHandler.java
@@ -4,14 +4,21 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Oauth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+  @Value("${base_url}")
+  private String BASE_URL;
+
 
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -26,6 +33,19 @@ public class Oauth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     if (auth != null) {
       System.out.println("auth.getName() = " + auth.getName());
     }
+
+    String requestURI = request.getRequestURI();
+    System.out.println("requestURI = " + requestURI);
+    HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+    SavedRequest savedRequest = requestCache.getRequest(request, response);
+    if (savedRequest != null) {
+      String redirectUrl = savedRequest.getRedirectUrl();
+      System.out.println("redirectUrl = " + redirectUrl);
+      response.sendRedirect(redirectUrl);
+    }
+
+    response.sendRedirect(BASE_URL);
+
 
     super.onAuthenticationSuccess(request, response, authentication);
   }
