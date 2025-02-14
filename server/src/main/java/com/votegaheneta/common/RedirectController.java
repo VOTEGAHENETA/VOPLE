cf8291e2125e32b1613046ab733f70a59a7c77c3
최효재 <hyojae5945@gmail.com>
2025-02-14 13:39:11 +0900

    fix : RedirectController ResponseEntity에 Forbidden 설정

--- server/src/main/java/com/votegaheneta/common/RedirectController.java
+++ server/src/main/java/com/votegaheneta/common/RedirectController.java
@@ -1,7 +1,9 @@
 package com.votegaheneta.common;
 
-import com.votegaheneta.common.response.ApiResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedirectController {
 
   @GetMapping("/elections/{sessionId}/question")
-  public ApiResponse<Boolean> redirectQuestion(@PathVariable Long sessionId) {
-//    HttpHeaders headers = new HttpHeaders();
-//    String redirectURL = String.format("/elections/%d/question", sessionId);
-    return ApiResponse.success(HttpStatus.FORBIDDEN, "질문 페이지로 리다이렉트", true);
+  public ResponseEntity<Void> redirectQuestion(@PathVariable Long sessionId) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setAccessControlAllowOrigin("http://localhost:5173");
+    headers.setAccessControlRequestMethod(HttpMethod.GET);
+    String redirectURL = String.format("/elections/%d/question", sessionId);
+    return new ResponseEntity<>(headers, HttpStatus.FORBIDDEN);
   }
 }
