5e67b4bf84d4aeb8668291f169bc257371a98d66
PoeySK <piz115@naver.com>
2025-02-14 16:44:30 +0900

    fix: 불필요한 user_id 파라미터 제거거

--- client/src/services/hooks/useGetUserInfo.ts
+++ client/src/services/hooks/useGetUserInfo.ts
@@ -1,10 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { getUserInfo } from '../user';
 
-export const useGetUserInfo = (userId: string) => {
+export const useGetUserInfo = () => {
   return useQuery({
-    queryKey: ['useUserInfo', userId],
-    queryFn: async () => getUserInfo(userId),
-    enabled: !!userId,
+    queryKey: ['useUserInfo'],
+    queryFn: async () => getUserInfo(),
   });
 };
