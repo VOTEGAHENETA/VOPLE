32887e13ca24e858affe25ce009294f4349d949f
최효재 <hyojae5945@gmail.com>
2025-02-16 16:53:21 +0900

    fix : 메소드 보안 예외 리턴타입 ResponseEntity로 수정 + 응답코드 403, 전역 예외 핸들러 전부 ResponseEntity로 반환
    프론트엔드에서 API 요청 응답을 Error에서 처리할 수 있게

--- server/src/main/java/com/votegaheneta/common/exception/handler/UserExceptionHandler.java
+++ server/src/main/java/com/votegaheneta/common/exception/handler/UserExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.votegaheneta.common.exception.handler;
 
 import com.votegaheneta.common.exception.EmptyOauthUserException;
-import com.votegaheneta.common.response.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class UserExceptionHandler {
 
   @ExceptionHandler(EmptyOauthUserException.class)
-  public ApiResponse<String> handleEmptyOauthUserException(EmptyOauthUserException e) {
-    return ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+  public ResponseEntity<String> handleEmptyOauthUserException(EmptyOauthUserException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
--- server/src/main/java/com/votegaheneta/security/handler/AuthorizationExceptionHandler.java
+++ server/src/main/java/com/votegaheneta/security/handler/AuthorizationExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.votegaheneta.security.handler;
 
-import com.votegaheneta.common.response.ApiResponse;
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationResult;
 import org.springframework.security.authorization.method.MethodAuthorizationDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -15,6 +15,6 @@ public class AuthorizationExceptionHandler implements MethodAuthorizationDeniedH
                                        AuthorizationResult authorizationResult) {
     String methodName = methodInvocation.getMethod().getName();
     String errMsg = String.format("%s 메소드에서 에러 발생함", methodName);
-    return ApiResponse.fail(HttpStatus.FORBIDDEN, errMsg);
+    return new ResponseEntity<String>(errMsg, HttpStatus.FORBIDDEN);
   }
 }
